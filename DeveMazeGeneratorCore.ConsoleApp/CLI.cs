1f27a1ad13a7d25fdc55ed72f1d1eedbcb43e10a
Brenton 'B-Train' Fletcher <i@bloople.net>
2026-04-13 19:55:07 +1000

    Refactor CLI

---
+++ DeveMazeGeneratorCore.ConsoleApp/CLI.cs
@@ -0,0 +1,107 @@
+using DeveMazeGeneratorCore.Extensions;
+using DeveMazeGeneratorCore.Mazes;
+using DeveMazeGeneratorCore.Serializers;
+
+namespace DeveMazeGeneratorCore.ConsoleApp;
+
+public class CLI(Options options)
+{
+    private string? mazeFileName;
+    private IMaze? maze;
+    private string? pathFileName;
+    private MazePath? path;
+
+    public async Task Run()
+    {
+        var tasks = new List<Func<Task>>();
+        while(options.HasNext()) tasks.Add(CreateTask(options.Next()));
+        foreach(var task in tasks) await task();
+    }
+
+    private Func<Task> CreateTask(string task) => task switch
+    {
+        "generate" => GenerateTask(),
+        "verify" => VerifyTask(),
+        "solve" => SolveTask(),
+        "render" => RenderTask(),
+        "render-path" => RenderPathTask(),
+        "benchmark" => BenchmarkTask(),
+        _ => throw new InvalidOperationException($"Unknown task: {task}"),
+    };
+
+    private Func<Task> GenerateTask()
+    {
+        var width = options.NextInt().MakeUneven();
+        var height = options.NextInt().MakeUneven();
+        int? seed = options.HasNextInt() ? options.NextInt() : null;
+        mazeFileName = options.NextFileName($"{Environment.TickCount}.maze");
+
+        return async () =>
+        {
+            maze = DeveMazeGeneratorCore.Generate(width, height, seed);
+            await MazeSerializer.Save(mazeFileName, maze);
+            Console.WriteLine($"Saved maze to {mazeFileName}");
+        };
+    }
+
+    private Func<Task> VerifyTask()
+    {
+        mazeFileName ??= options.Next();
+        return async () =>
+        {
+            maze ??= await MazeSerializer.Load(mazeFileName);
+            var result = Verifier.IsPerfectMaze(maze);
+            Console.WriteLine($"Is our maze perfect?: {result}");
+        };
+    }
+
+    private Func<Task> SolveTask()
+    {
+        mazeFileName ??= options.Next();
+        pathFileName = options.NextFileName(Path.ChangeExtension(mazeFileName, ".path"));
+        return async () =>
+        {
+            maze ??= await MazeSerializer.Load(mazeFileName);
+            path = PathFinder.Find(maze);
+            await MazePathSerializer.Save(pathFileName, path);
+            Console.WriteLine($"Saved solution to {pathFileName}");
+        };
+    }
+
+    private Func<Task> RenderTask()
+    {
+        mazeFileName ??= options.Next();
+        var imageFileName = options.NextFileName(Path.ChangeExtension(mazeFileName, ".png"));
+        return async () =>
+        {
+            maze ??= await MazeSerializer.Load(mazeFileName);
+            using var image = ImageCreator.CreateImage(maze);
+            await ImageCreator.Save(imageFileName, image);
+
+            Console.WriteLine($"Saved maze image to {imageFileName}");
+        };
+    }
+
+    private Func<Task> RenderPathTask()
+    {
+        mazeFileName ??= options.Next();
+        pathFileName ??= options.Next();
+        var imageFileName = options.NextFileName(Path.ChangeExtension(pathFileName, ".path.png"));
+        return async () =>
+        {
+            maze ??= await MazeSerializer.Load(mazeFileName);
+            path ??= await MazePathSerializer.Load(pathFileName);
+            using var image = ImageCreator.CreateImage(maze, path);
+            await ImageCreator.Save(imageFileName, image);
+
+            Console.WriteLine($"Saved maze with solution image to {imageFileName}");
+        };
+    }
+
+    private Func<Task> BenchmarkTask() => async () =>
+    {
+        var maze = DeveMazeGeneratorCore.BenchmarkBaseline();
+        var result = Verifier.IsPerfectMaze(maze);
+        if(!result) throw new InvalidOperationException("Maze is not perfect");
+    };
+}
--- DeveMazeGeneratorCore.ConsoleApp/Program.cs
+++ DeveMazeGeneratorCore.ConsoleApp/Program.cs
@@ -1,22 +1,4 @@
 using DeveMazeGeneratorCore.ConsoleApp;
 
-var options = new Options(args);
-var context = new JobContext();
-
-IJob ParseJob(string job)
-{
-    switch(job)
-    {
-        case "generate": return GenerateJob.Parse(options, context);
-        case "verify": return VerifyJob.Parse(options, context);
-        case "solve": return SolveJob.Parse(options, context);
-        case "render": return RenderJob.Parse(options, context);
-        case "render-path": return RenderPathJob.Parse(options, context);
-        case "benchmark": return BenchmarkJob.Parse(options, context);
-        default: throw new InvalidOperationException($"Unknown task: {job}");
-    }
-}
-
-var jobs = new List<IJob>();
-while(options.HasNext()) jobs.Add(ParseJob(options.Next()));
-foreach(var job in jobs) await job.Run(context);
+var cli = new CLI(new Options(args));
+await cli.Run();
