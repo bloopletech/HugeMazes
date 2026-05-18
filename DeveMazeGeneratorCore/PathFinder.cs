a6fe53f7a19ba173baf75b81c77bb70a76004973
Brenton "B-Train" Fletcher <i@bloople.net>
2026-05-18 15:04:12 +1000

    wip

--- DeveMazeGeneratorCore/PathFinder.cs
+++ DeveMazeGeneratorCore/PathFinder.cs
@@ -25,97 +25,132 @@ public static class PathFinder
         var width = maze.Width;
         var height = maze.Height;
 
-        var points = new List<MazePoint>()
-        {
-            start
-        };
+        var path = new MazePath(width, height);
+        path[start.X, start.Y] = true;
 
-        MazePoint cur;
+        var corners = new List<MazePoint>();
+
+        Span<MazePoint> entrances = stackalloc MazePoint[4];
+        entrances.Clear();
+
+        MazePoint cur = start;
         MazePoint prev = new(-1, -1);
+        MazePoint prevPrev = new(-1, -1);
+        MazePoint next = new(-1, -1);
 
         var lastBackTrackDir = -1;
 
-        while(points.Count != 0)
+        while(true)
         {
-            cur = points[^1];
+            Console.WriteLine($"cur: {cur}, prev: {prev}, prevPrev: {prevPrev}, next: {next}, lastBackTrackDir: {lastBackTrackDir}");
             var (x, y) = cur;
 
             if(x == end.X && y == end.Y)
             {
+                Console.WriteLine("Path traced");
                 //Path found
                 break;
             }
 
+            var entranceCount = 0;
+            if(x - 1 > 0 && maze[x - 1, y])
+            {
+                entrances[entranceCount].Set(x - 1, y);
+                entranceCount++;
+            }
+            if(x + 1 < width - 1 && maze[x + 1, y])
+            {
+                entrances[entranceCount].Set(x + 1, y);
+                entranceCount++;
+            }
+            if(y - 1 > 0 && maze[x, y - 1])
+            {
+                entrances[entranceCount].Set(x, y - 1);
+                entranceCount++;
+            }
+            if(y + 1 < height - 1 && maze[x, y + 1])
+            {
+                entrances[entranceCount].Set(x, y + 1);
+                entranceCount++;
+            }
+
+            if(entranceCount > 2 && corners[^1] != cur) corners.Add(cur);
+
+
             //Make sure the point was not the previous point, also make sure that if we backtracked we don't go to a direction we already went to, also make sure that the point is white
             if((prev.X != x + 1 || prev.Y != y) && lastBackTrackDir < 0 && x + 1 < width - 1 && maze[x + 1, y])
             {
-                points.Add(new(x + 1, y));
+                next = new(x + 1, y);
+                path[next.X, next.Y] = true;
                 lastBackTrackDir = -1;
+                prevPrev = prev;
                 prev = cur;
+                cur = next;
             }
             else if((prev.X != x || prev.Y != y + 1) && lastBackTrackDir < 1 && y + 1 < height - 1 && maze[x, y + 1])
             {
-                points.Add(new(x, y + 1));
+                next = new(x, y + 1);
+                path[next.X, next.Y] = true;
                 lastBackTrackDir = -1;
+                prevPrev = prev;
                 prev = cur;
+                cur = next;
             }
             else if((prev.X != x - 1 || prev.Y != y) && lastBackTrackDir < 2 && x - 1 > 0 && maze[x - 1, y])
             {
-                points.Add(new(x - 1, y));
+                next = new(x - 1, y);
+                path[next.X, next.Y] = true;
                 lastBackTrackDir = -1;
+                prevPrev = prev;
                 prev = cur;
+                cur = next;
             }
             else if((prev.X != x || prev.Y != y - 1) && lastBackTrackDir < 3 && y - 1 > 0 && maze[x, y - 1])
             {
-                points.Add(new(x, y - 1));
+                next = new(x, y - 1);
+                path[next.X, next.Y] = true;
                 lastBackTrackDir = -1;
+                prevPrev = prev;
                 prev = cur;
+                cur = next;
             }
             else
             {
-                var prepoppy = points[^1];
-                points.RemoveAt(points.Count - 1);
-
-                if(points.Count == 0)
+                if(x == -1 && y == -1)
                 {
+                    Console.WriteLine("No path can be traced");
                     //No path found
                     break;
                 }
 
-                var newcur = points[^1];
+                path[x, y] = false;
 
-                //Set the new previous point
-                if(points.Count == 1)
-                {
-                    prev = new MazePoint(-1, -1);
-                }
-                else
-                {
-                    prev = points.ElementAt(points.Count - 2);
-                }
+
+
 
                 //Set the direction we backtracked from
-                if(prepoppy.X > newcur.X)
+                if(cur.X > prev.X)
                 {
                     lastBackTrackDir = 0;
                 }
-                else if(prepoppy.Y > newcur.Y)
+                else if(cur.Y > prev.Y)
                 {
                     lastBackTrackDir = 1;
                 }
-                else if(prepoppy.X < newcur.X)
+                else if(cur.X < prev.X)
                 {
                     lastBackTrackDir = 2;
                 }
-                else if(prepoppy.Y < newcur.Y)
+                else if(cur.Y < prev.Y)
                 {
                     lastBackTrackDir = 3;
                 }
+
+                //Set the new previous point
+                prev = prevPrev;
             }
         }
 
-        var path = new MazePath(width, height);
-        foreach(var point in points) path[point.X, point.Y] = true;
         return path;
     }
 }
