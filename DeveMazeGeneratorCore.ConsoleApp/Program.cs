fc132c29182bb3fd549774a1ab1096e66b40f7cd
Brenton "B-Train" Fletcher <i@bloople.net>
2026-06-25 14:20:33 +1000

    Merge CLI into Program

--- DeveMazeGeneratorCore.ConsoleApp/Program.cs
+++ DeveMazeGeneratorCore.ConsoleApp/Program.cs
@@ -1,10 +1,207 @@
 using DeveMazeGeneratorCore.ConsoleApp;
+using DeveMazeGeneratorCore.Extensions;
 using DeveMazeGeneratorCore.IO;
+using DeveMazeGeneratorCore.Mazes;
+using DeveMazeGeneratorCore.Paths;
+using static DeveMazeGeneratorCore.DeveMazeGeneratorCore;
+using static DeveMazeGeneratorCore.Verifier;
 
-//generate 16384 16384 verify solve verify-path render render-path
-
-CLI.SkipReuse = true;
+var skipReuse = true;
 IStore.LongOverride = true;
 
-var cli = new CLI(new Options(args));
-cli.Run();
+var options = new Options(args);
+
+if(!options.HasNext()) options = new(["help"]);
+if(options.HasNextInt())
+{
+    var width = options.Next();
+    var height = options.HasNext() ? options.Next() : width;
+    options = new(["generate", width, height, "verify", "solve", "verify-path", "render", "render-path"]);
+}
+
+string? mazeFileName = null;
+IMaze? maze = null;
+string? pathFileName = null;
+IMazePath? path = null;
+
+Action CreateTask(string task) => task switch
+{
+    "help" => HelpTask(),
+    "generate" => GenerateTask(),
+    "verify" => VerifyTask(),
+    "solve" => SolveTask(),
+    "verify-path" => VerifyPathTask(),
+    "render" => RenderTask(),
+    "render-path" => RenderPathTask(),
+    "benchmark" => BenchmarkTask(),
+    "benchmark-direction-maze-path" => BenchmarkDirectionMazePathTask(),
+    _ => throw new InvalidOperationException($"Unknown task: {task}"),
+};
+
+static Action HelpTask() => () => Console.Write("""
+    Provide list of tasks to perform as command line arguments.
+    Available tasks:
+
+    generate <width> <height> [example.maze]
+        Generate a random maze of given width and height and save maze to example.maze
+        If no filename provided, defaults to <random number>.maze
+
+    verify <example.maze>
+        Verify that given maze is valid
+
+    solve <example.maze> [example.path]
+        Solve given maze and save solution to example.path
+        If no path filename provided, defaults to <maze file basename>.path
+
+    verify-path <example.maze> <example.path>
+        Verify that given solution is valid
+
+    render <example.maze> [example.ppm]
+        Draw given maze and save image to example.ppm
+        If no image filename provided, defaults to <maze file basename>.ppm
+
+    render-path <example.maze> <example.path> [example.path.ppm]
+        Draw given maze, then draw given solution on top, and then save image to example.path.ppm
+        If no image filename provided, defaults to <maze file basename>.path.ppm
+
+    You can chain together tasks, and in that case, you don't have to repeat the same filenames.
+    Example:
+
+    generate 16384 16384 interesting.maze verify solve verify-path render render-path
+
+    Will create the following files:
+
+    interesting.maze
+        The generated maze
+
+    interesting.path
+        The solution to the maze
+
+    interesting.ppm
+        The maze rendered as an image
+
+    interesting.path.ppm
+        The maze and solution rendered as an image
+    """);
+
+Action GenerateTask()
+{
+    var width = options.NextInt().MakeUneven();
+    var height = options.NextInt().MakeUneven();
+    int? seed = options.HasNextInt() ? options.NextInt() : null;
+    mazeFileName = options.NextFileName($"{Environment.TickCount}.maze");
+
+    return () =>
+    {
+        maze = Generate(width, height, seed, new StreamStore(mazeFileName));
+        maze.Write();
+        Console.WriteLine($"Saved maze to {mazeFileName}");
+    };
+}
+
+Action VerifyTask()
+{
+    mazeFileName ??= options.Next();
+    return () =>
+    {
+        maze ??= Load(mazeFileName);
+        var result = IsPerfectMaze(maze);
+        Console.WriteLine($"Is our maze perfect?: {result}");
+    };
+}
+
+Action SolveTask()
+{
+    mazeFileName ??= options.Next();
+    pathFileName = options.NextFileName(Path.ChangeExtension(mazeFileName, ".path"));
+    return () =>
+    {
+        maze ??= Load(mazeFileName);
+        path = Solve(maze, new StreamStore(pathFileName));
+        path.Write();
+        Console.WriteLine($"Saved path to {pathFileName}");
+    };
+}
+
+Action VerifyPathTask()
+{
+    mazeFileName ??= options.Next();
+    pathFileName ??= options.Next();
+    return () =>
+    {
+        maze ??= Load(mazeFileName);
+        path ??= LoadPath(pathFileName);
+        var result = IsPerfectPath(maze, path);
+        Console.WriteLine($"Is our path perfect?: {result}");
+    };
+}
+
+Action RenderTask()
+{
+    mazeFileName ??= options.Next();
+    var imageFileName = options.NextFileName(Path.ChangeExtension(mazeFileName, ".ppm"));
+    return () =>
+    {
+        maze ??= Load(mazeFileName);
+        using var image = Render(maze, new StreamStore(imageFileName));
+        image.Write();
+        Console.WriteLine($"Saved maze image to {imageFileName}");
+    };
+}
+
+Action RenderPathTask()
+{
+    mazeFileName ??= options.Next();
+    pathFileName ??= options.Next();
+    var imageFileName = options.NextFileName(Path.ChangeExtension(pathFileName, ".path.ppm"));
+    return () =>
+    {
+        maze ??= Load(mazeFileName);
+        path ??= LoadPath(pathFileName);
+        using var image = Render(maze, path, new StreamStore(imageFileName));
+        image.Write();
+        Console.WriteLine($"Saved maze with path image to {imageFileName}");
+    };
+}
+
+static Action BenchmarkTask() => () =>
+{
+    var maze = BenchmarkBaseline();
+    var result = IsPerfectMaze(maze);
+    if(!result) throw new InvalidOperationException("Maze is not perfect");
+};
+
+static Action BenchmarkDirectionMazePathTask() => () =>
+{
+    var maze = BenchmarkBaseline();
+    var path = Solve(maze);
+    var result = IsPerfectPath(maze, path);
+    if(!result) throw new InvalidOperationException("Path is not perfect");
+};
+
+static IMaze Load(string fileName) => MazeSerializer.Read(new StreamStore(fileName));
+
+static IMazePath LoadPath(string fileName) => MazePathSerializer.Read(new StreamStore(fileName));
+
+try
+{
+    var tasks = new List<Action>();
+    while(options.HasNext()) tasks.Add(CreateTask(options.Next()));
+    foreach(var task in tasks)
+    {
+        task();
+
+        if(skipReuse)
+        {
+            maze?.Dispose();
+            maze = null;
+            path?.Dispose();
+            path = null;
+        }
+    }
+}
+finally
+{
+    maze?.Dispose();
+    path?.Dispose();
+}
