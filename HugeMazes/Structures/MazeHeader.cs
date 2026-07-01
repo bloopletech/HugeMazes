59461a21ec531f90f26047a1448f1ff3fdbb4e08
Brenton "B-Train" Fletcher <i@bloople.net>
2026-07-01 12:59:28 +1000

    Use both binary and human readable magic numbers

--- HugeMazes/Structures/MazeHeader.cs
+++ HugeMazes/Structures/MazeHeader.cs
@@ -5,7 +5,7 @@ using HugeMazes.Mazes;
 namespace HugeMazes.Structures;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
-public record struct MazeHeader(long Magic, ushort Version, MazeType MazeType)
+public record struct MazeHeader(long MagicHuman, long MagicBinary, ushort Version, MazeType MazeType)
 {
     public static readonly int SizeOf = IStore.SizeOf<MazeHeader>();
 }
