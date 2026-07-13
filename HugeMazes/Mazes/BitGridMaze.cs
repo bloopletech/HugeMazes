9c1a34a97a4ca8b72a903549730d68da0d5d275c
Brenton "B-Train" Fletcher <bfletcher@dijones.com.au>
2026-07-13 13:18:26 +1000

    Simplify Maze implementations by merging *BitGrid classes into the Maze classes

--- HugeMazes/Mazes/BitGridMaze.cs
+++ HugeMazes/Mazes/BitGridMaze.cs
@@ -1,45 +1,72 @@
+using System.Collections;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using HugeMazes.Extensions;
 using HugeMazes.IO;
 using HugeMazes.Structures;
 
 namespace HugeMazes.Mazes;
 
-public sealed class BitGridMaze(BitGrid grid) : IMaze
+public class BitGridMaze : Storable, IMaze
 {
-    public BitGridMaze(IStore store, bool leaveOpen = false) : this(new BitGrid(store, leaveOpen))
+    private BitArray array;
+    private MazeSize size;
+
+    public BitGridMaze(IStore store, bool leaveOpen = false) : base(store, leaveOpen)
     {
+        array = null!;
     }
 
-    public BitGridMaze(IStore store, MazeSize size, bool leaveOpen = false) : this(new BitGrid(store, size, leaveOpen))
+    public BitGridMaze(IStore store, MazeSize size, bool leaveOpen = false) : base(store, leaveOpen)
     {
+        this.size = size;
+        array = new((int)size.Area);
     }
 
-    public IStore Store => grid.Store;
-    public bool IsLong => grid.IsLong;
-    public long Extent => grid.Extent;
-    public MazeSize Size => grid.Size;
-    public int Width => grid.Width;
-    public int Height => grid.Height;
+    public override long Extent => CollectionsMarshal.AsBytes(array).Length + MazeSize.SizeOf;
+    public MazeSize Size => size;
+    public int Width => size.Width;
+    public int Height => size.Height;
 
     public bool this[int x, int y]
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => grid[x, y];
-
+        get => array[Index(x, y)];
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        set => grid[x, y] = value;
+        set => array[Index(x, y)] = value;
     }
 
-    public void Read() => grid.Read();
-
-    public void Write() => grid.Write();
-
-    public void Dispose() => grid.Dispose();
-
-    public IMaze Clone() => Clone(IStore.Create(IsLong));
-
-    public IMaze Clone(IStore destination, bool leaveOpen = false)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private int Index(int x, int y)
     {
-        return new BitGridMaze(grid.Clone(destination, leaveOpen));
+        if((uint)x >= (uint)size.Width) ExceptionExtensions.ThrowOutOfRangeException(x);
+        if((uint)y >= (uint)size.Height) ExceptionExtensions.ThrowOutOfRangeException(y);
+        return x + (y * size.Width);
+    }
+
+    public override void Read()
+    {
+        size = store.Read<MazeSize>(0);
+        array = new((int)size.Area);
+        store.ReadExactly(MazeSize.SizeOf, CollectionsMarshal.AsBytes(array));
+    }
+
+    public override void Write()
+    {
+        store.Write(0, size);
+        store.Write(MazeSize.SizeOf, CollectionsMarshal.AsBytes(array));
+    }
+
+    IMaze IMaze.Clone() => Clone();
+    public BitGridMaze Clone() => Clone(IStore.Create(IsLong));
+
+    IMaze IMaze.Clone(IStore destination, bool leaveOpen) => Clone(destination, leaveOpen);
+    public BitGridMaze Clone(IStore destination, bool leaveOpen = false)
+    {
+        Write();
+        store.CopyTo(destination);
+        var result = new BitGridMaze(destination, leaveOpen);
+        result.Read();
+        return result;
     }
 }
